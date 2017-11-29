acc55caf2f10a394c9b4116e16c0a78f2f810882
PH <1836071395@qq.com>
2017-11-29 17:30:46 +0800

    Signed-off-by: PH <1836071395@qq.com>
    重新设计寻路算法
    
    Signed-off-by: PH <1836071395@qq.com>

--- findPath/findPath2.js
+++ findPath/findPath2.js
@@ -10,305 +10,332 @@
  *     
  */
 
+(function (window) {
 
-var globleID = 0;
-/**
-* 二维空间坐标。
-* @param  x  x 轴坐标
-* @param  y  y 轴坐标 
-*/
-var Vectort2 = function Vectort2(x, y) {
-    this.x = x;
-    this.y = y;
-    this.id = globleID++;
-};
+    var globleID = 0;
+   
+    /**
+     * 二维空间坐标。
+     * @param { number } x  x 轴坐标.
+     * @param { number } y  y 轴坐标 .
+     * @param { bool } isDynamic 是否为动态点.
+     */
+    var Vectort2 = function Vectort2(x, y, isDynamic) {
+        this.x = x;
+        this.y = y;
+        this.id = globleID++;
+        this.isDynamic = isDynamic;
+    };
 
 
-/**
- * 三角形,因使用 y=kx+b 表示各边直线.暂时只支持二位空间点.
- * @param  pointA 点A
- * @param  pointB 点B
- * @param  pointC 点C
- */
-var Trangle = function Trangle(pointA, pointB, pointC) {
-    this.pointA = pointA;
-    this.pointB = pointB;
-    this.pointC = pointC;
-    this.kca = (pointA.y - pointC.y) / (pointA.x - pointC.x);
-    this.kcb = (pointB.y - pointC.y) / (pointB.x - pointC.x);
-    this.kab = (pointB.y - pointA.y) / (pointB.x - pointA.x);
-    this.bca = pointC.y - this.kca * pointC.x;
-    this.bcb = pointC.y - this.kcb * pointC.y;
-    this.bab = pointA.y - this.kab * pointA.y;
-};
-/**
- * 判断二维点是否在三角形内，只支持二维空间点.
- * @param  point  任一点.
- */
-Trangle.prototype.isPointIn = function isPointIn(point) {
-    var xArray = [this.pointA.x, this.pointB.x, this.pointC.x];
-    xArray = xArray.sort();
-    if (point.x < xArray[0] || point.x > xArray[2]) {
-        return false;
-    }
-
-    if (point.equalse(this.pointA) || point.equalse(this.pointB) || point.equalse(this.pointC)) {
-        return true;
-    }
-    var yValues = [];
-    yValues.push(this.kab * point.x + this.bab);
-
-    yValues.push(this.kcb * point.x + this.bcb);
-    yValues.push(this.kca * point.x + this.bca);
-
-    var times = 0;
-    for (var i = yValues.length - 1; i >= 0; i--) {
-        if (yValues[i] < point.y) {
-            times++;
+    /**
+     * 三角形,因使用 y=kx+b 表示各边直线.暂时只支持二位空间点.
+     * @param  pointA 点A
+     * @param  pointB 点B
+     * @param  pointC 点C
+     */
+    var Trangle = function Trangle(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+        this.kca = (pointA.y - pointC.y) / (pointA.x - pointC.x);
+        this.kcb = (pointB.y - pointC.y) / (pointB.x - pointC.x);
+        this.kab = (pointB.y - pointA.y) / (pointB.x - pointA.x);
+        this.bca = pointC.y - this.kca * pointC.x;
+        this.bcb = pointC.y - this.kcb * pointC.y;
+        this.bab = pointA.y - this.kab * pointA.y;
+    };
+    /**
+     * 判断二维点是否在三角形内，只支持二维空间点.
+     * @param  point  任一点.
+     */
+    Trangle.prototype.isPointIn = function isPointIn(point) {
+        var xArray = [this.pointA.x, this.pointB.x, this.pointC.x];
+        xArray = xArray.sort();
+        if (point.x < xArray[0] || point.x > xArray[2]) {
+            return false;
         }
-    }
 
-    return times % 2 == 1;
-};
+        if (point.equalse(this.pointA) || point.equalse(this.pointB) || point.equalse(this.pointC)) {
+            return true;
+        }
+        var yValues = [];
+        yValues.push(this.kab * point.x + this.bab);
+
+        yValues.push(this.kcb * point.x + this.bcb);
+        yValues.push(this.kca * point.x + this.bca);
+
+        var times = 0;
+        for (var i = yValues.length - 1; i >= 0; i--) {
+            if (yValues[i] < point.y) {
+                times++;
+            }
+        }
+
+        return times % 2 == 1;
+    };
 
 
 
-/**
- * a=>b单向线段.由两个点组成，描述了地图中所有点的联通情况.
- * @param  nodeA a点
- * @param  nodeB b点
- * @param  dist  a点与b点距离
- * @param  isDynamic 是否为动态的点
- */
-var Segment = function Segment(nodeA, nodeB, isDynamic, dist) {
-    this.isDynamic = isDynamic;
-    if (nodeB instanceof Vectort2 && nodeA instanceof Vectort2) {
-        // this.nodeAName = nodeA.getName();
-        // this.nodeBName = nodeB.getName();
-        this.nodeA = nodeA;
-        this.nodeB = nodeB;
-        var dx = nodeA.x - nodeB.x;
-        var dy = nodeA.y - nodeB.y;
-        this.dist = Math.pow(dx * dx + dy * dy, 1 / 2);
-    } else if (nodeA instanceof Vectort3 && nodeB instanceof Vectort3) {
-        // this.nodeAName = nodeA.getName();
-        // this.nodeBName = nodeB.getName();
-        this.nodeA = nodeA;
-        this.nodeB = nodeB;
-        var dx = nodeB.x - nodeA.x;
-        var dy = nodeB.y - nodeA.y;
-        var dz = nodeB.z - nodeB.z;
-        this.dist = Math.pow(dx * dx + dy * dy + dz * dz, 1 / 3);
-    } else {
-        this.nodeAName = nodeA;
-        this.nodeBName = nodeB;
-        this.dist = dist;
-        this.isDynamic = false;
-    }
-};
-/**
- * 获取两点之间的距离.暂时只支持2D.
- */
-Segment.prototype.getDist = function () {
-    if (!this.isDynamic) {
-        this.getDist = function () {
+    /**
+     * a=>b单向线段.由两个点组成，描述了地图中所有点的联通情况.
+     * @param  nodeA a点
+     * @param  nodeB b点
+     * @param  dist  a点与b点距离
+     */
+    var Segment = function Segment(nodeA, nodeB, dist) {
+        this.isDynamic =nodeA.isDynamic||nodeB.isDynamic;
+        if (nodeB instanceof Vectort2 && nodeA instanceof Vectort2) {
+
+            this.nodeA = nodeA;
+            this.nodeB = nodeB;
+            var dx = nodeA.x - nodeB.x;
+            var dy = nodeA.y - nodeB.y;
+            this.dist = Math.pow(dx * dx + dy * dy, 1 / 2);
+        } else if (nodeA instanceof Vectort3 && nodeB instanceof Vectort3) {
+
+            this.nodeA = nodeA;
+            this.nodeB = nodeB;
+            var dx = nodeB.x - nodeA.x;
+            var dy = nodeB.y - nodeA.y;
+            var dz = nodeB.z - nodeB.z;
+            this.dist = Math.pow(dx * dx + dy * dy + dz * dz, 1 / 3);
+        } else {
+            this.nodeAName = nodeA;
+            this.nodeBName = nodeB;
+            this.dist = dist;
+            this.isDynamic = false;
+        }
+    };
+    /**
+     * 获取两点之间的距离.暂时只支持2D.
+     */
+    Segment.prototype.getDist = function () {
+        if (!this.isDynamic) {
+            this.getDist = function () {
+                return this.dist;
+            }
             return this.dist;
-        }
-        return this.dist;
-    } else {
-        this.getDist = function () {
-            return Math.pow(dx * dx + dy * dy, 1 / 2);
-        }
-        return Math.pow(dx * dx + dy * dy, 1 / 2);
-    }
-}
-
-
-
-/**
- * 地图.
- */
-var MapData = function (obj) {
-
-    this.SPLIT_SYMBOL = "|";
-    this.MAX = Infinity;
-    this.nodeToRelatedMap = {};
-    this.segmentMap = {};
-
-}
-MapData.prototype.getNodes = function (node) {
-    return this.nodeToRelatedMap[node];
-}
-MapData.prototype.getDist = function (nodeA, nodeB) {
-    var segment = this.segmentMap[nodeA.id + this.SPLIT_SYMBOL + nodeB.id];
-    if (segment) {
-        return segment.getDist();
-    } else {
-        return this.MAX;
-    }
-}
-/**
- * 
- * @param  segment  添加线段到地图.
- */
-MapData.prototype.add = function (segment) {
-    var list = this.nodeToRelatedMap[segment.nodeA];
-    if (list) {
-        list.push(segment.nodeB);
-    } else {
-        list = [segment.nodeB];
-        this.nodeToRelatedMap[segment.nodeA] = list;
-    }
-    this.segmentMap[segment.nodeA.id + this.SPLIT_SYMBOL + segment.nodeB.id] = segment;
-    this.segmentMap[segment.nodeB.id + this.SPLIT_SYMBOL + segment.nodeA.id] = segment;
-}
-
-
-
-
-/**
- * 点与点构成的路径树.
- * @param  node 当前点.
- * @param  rootTree 起始点.
- */
-var NodeTree = function (node, rootTree) {
-    //当前点.
-    this.node = node;
-    //地图.
-    this.mapData = rootTree.mapData;
-    //根节点.
-    this.rootTree = rootTree;
-
-    //当前点距根节点的距离.
-    this.toRootNodeDist = 0;
-    //父节点.
-    this.parentTree;
-    //子节点.
-    this.childrenTree = [];
-}
-NodeTree.prototype.appendTree = function () {
-
-    var nodeList = this.mapData.getNodes(this.node);
-    for (var i = nodeList.length - 1; i >= 0; i--) {
-        var nextTree = new NodeTree(nodeList[i], this.rootTree);
-        if (this._addTree(nextTree)) {
-            nextTree.appendTree();
+        } else {
+            this.getDist = function () {
+                var dx = this.nodeA.x - this.nodeB.x;
+                var dy = this.nodeA.y - this.nodeB.y;
+                return Math.pow(dx * dx + dy * dy, 1 / 2);
+            }
+            return this.getDist();
         }
     }
-};
-/**
- * 添加节点，若树中已存在更短的节点，不再添加，返回false.
- * @param  nextTree 
- */
-NodeTree.prototype._addTree = function (nextTree) {
 
-    if (!nextTree.node) {
-        throw "Error";
-    }
-    var lastDist = this.rootTree.getDistToPoint(nextTree.node);
-    var curDist = this.toRootNodeDist + this.mapData.getDist(this.node, nextTree.node);
 
-    if (isNaN(lastDist) || lastDist > curDist) {
 
-        nextTree.toRootNodeDist = curDist;
-        nextTree.parentTree = this;
-        this.childrenTree.push(nextTree);
+    /**
+     * 地图.
+     */
+    var MapData = function (obj) {
 
-        this.rootTree.addTree(nextTree);
-        return true;
-    }
-}
+        this.SPLIT_SYMBOL = "|";
+        this.MAX = Infinity;
+        this.nodeToRelatedMap = {};
+        this.segmentMap = {};
+        //地图区域的三角形.
+        this.trangleList = [];
 
-var RootNodeTree = function (node, mapData) {
+        if (obj instanceof MapData) {
+            //Copy of MapData.
 
-    this.mapData = mapData;
-    NodeTree.apply(this, [node, this]);
-    this.nodeDistMap = {};
-    this.endTrees = [];
-    this.endNodes = [];
-}
-RootNodeTree.prototype = NodeTree.prototype;
+            for (var k in obj.nodeToRelatedMap) {
+                var list = obj.nodeToRelatedMap[k];
+                var copyList = [];
+                for (var i = list.length - 1; i >= 0; i--) {
+                    copyList.push(list[i]);
+                }
+                this.nodeToRelatedMap[k] = copyList;
 
-//通过node获取到根节点的距离.
-RootNodeTree.prototype.getDistToPoint = function (node) {
-    return this.nodeDistMap[node.id];
-}
-//通过node获取到根节点的距离.
-RootNodeTree.prototype._setDistToPoint = function (nodeTree) {
-    this.nodeDistMap[nodeTree.node.id] = nodeTree.toRootNodeDist;
-}
-//处理末端Tree.
-RootNodeTree.prototype._addEndNode = function (nodeTree) {
+                for (var i = obj.trangleList.length - 1; i >= 0; i--) {
+                    this.trangleList.push(obj.trangleList[i]);
+                }
+            }
 
-    var ind = this.endNodes.indexOf(nodeTree.node);
-    if (-1 === ind) {
-        this.endNodes.push(nodeTree.node);
-        this.endTrees.push(nodeTree);
-    } else {
-        this.endTrees[ind] = nodeTree;
-    }
-}
-//通过node获取到根节点的距离.
-RootNodeTree.prototype.addTree = function (nodeTree) {
-    this._setDistToPoint(nodeTree);
-    this._addEndNode(nodeTree);
-}
-//通过node获取到根节点的距离.
-RootNodeTree.prototype._getMinTree = function () {
-    if (this.endTrees.length === 0) {
-        throw "no path found.";
-    }
-    var result = this.endTrees[0];
-    for (var i = this.endTrees.length - 1; i >= 0; i--) {
-        var now = this.endTrees[i]
-        if (now.toRootNodeDist < result.toRootNodeDist) {
-            result = now;
         }
     }
-    return result;
-}
-RootNodeTree.prototype._loadParent = function (nodeTree, array) {
-    if (nodeTree) {
-        array.unshift(nodeTree.node);
-        this._loadParent(nodeTree.parentTree, array);
+    MapData.prototype.getNodes = function (node) {
+        return this.nodeToRelatedMap[node];
     }
-}
-RootNodeTree.prototype.getMinPathNodes = function () {
-    var result = [];
-    this._loadParent(this._getMinTree(), result);
-    return result;
-}
-
-
-
-//点与点。
-/**
- * 1.找与点相通的点,累加与根目录的距离.
- * 2.判断点是否到过且最短.
- * 3.找下一个点.
- */
-
-var map = new MapData();
-
-var pointA = new Vectort2(1, 0);
-var pointB = new Vectort2(2, 0);
-var pointC = new Vectort2(1, 2);
-var pointD = new Vectort2(2, 2);
-var pointE = new Vectort2(0, 2);
-var pointF = new Vectort2(0, 3);
-var pointG = new Vectort2(3, 3);
-var pointH = new Vectort2(3, 2);
-var list = [pointA, pointB, pointC, pointD, pointE, pointF, pointG, pointH];
-for (var i = 0; i < list.length - 1; i++) {
-    for (var j = i + 1; j < list.length; j++) {
-        map.add(new Segment(list[i], list[j]));
-        map.add(new Segment(list[j], list[i]));
+    MapData.prototype.getDist = function (nodeA, nodeB) {
+        var segment = this.segmentMap[nodeA.id + this.SPLIT_SYMBOL + nodeB.id];
+        if (segment) {
+            return segment.getDist();
+        } else {
+            return this.MAX;
+        }
     }
-}
+    MapData.prototype._addTrangle = function (segmet) {
+        for (var i = this.nodeNameList.length - 1; i >= 0; i--) {
+            var nodeName = this.nodeNameList[i].getName();
+            var keyA = nodeName + this.SPLIT_SYMBOL + segmet.nodeAName;
+            var keyB = nodeName + this.SPLIT_SYMBOL + segmet.nodeBName;
+            if (this.segmetsMap[keyB] && this.segmetsMap[keyA]) {
+
+                var trangle = new Trangle(segmet.pointA, segmet.pointB, this.nodeNameList[i]);
+                this.trangleList.push(trangle);
+            }
+        }
+    };
+    /**
+     * 
+     * @param  segment  添加线段到地图.
+     */
+    MapData.prototype.add = function (segment) {
+        var list = this.nodeToRelatedMap[segment.nodeA];
+        if (list) {
+            list.push(segment.nodeB);
+        } else {
+            list = [segment.nodeB];
+            this.nodeToRelatedMap[segment.nodeA] = list;
+        }
+        this.segmentMap[segment.nodeA.id + this.SPLIT_SYMBOL + segment.nodeB.id] = segment;
+        this.segmentMap[segment.nodeB.id + this.SPLIT_SYMBOL + segment.nodeA.id] = segment;
+    }
 
 
 
-var root = new RootNodeTree(pointA, map);
-root.appendTree();
-var nodes = root.getMinPathNodes();
-console.log(root);
+
+    /**
+     * 点与点构成的路径树.
+     * @param  node 当前点.
+     * @param  rootTree 起始点.
+     */
+    var NodeTree = function (node, rootTree) {
+        //当前点.
+        this.node = node;
+        //地图.
+        this.mapData = rootTree.mapData;
+        //根节点.
+        this.rootTree = rootTree;
+
+        //当前点距根节点的距离.
+        this.toRootNodeDist = 0;
+        //父节点.
+        this.parentTree;
+        //子节点.
+        this.childrenTree = [];
+    }
+    NodeTree.prototype.appendTree = function () {
+
+        var nodeList = this.mapData.getNodes(this.node);
+        for (var i = nodeList.length - 1; i >= 0; i--) {
+            var nextTree = new NodeTree(nodeList[i], this.rootTree);
+            if (this._addTree(nextTree) && nextTree.node.id !== this.rootTree.endNode.id) {
+                nextTree.appendTree();
+            }
+        }
+    };
+    /**
+     * 添加节点，若树中已存在更短的节点，不再添加，返回false.
+     * @param  nextTree 
+     */
+    NodeTree.prototype._addTree = function (nextTree) {
+
+        if (!nextTree.node) {
+            throw "Error";
+        }
+        var lastDist = this.rootTree.getDistToPoint(nextTree.node);
+        var curDist = this.toRootNodeDist + this.mapData.getDist(this.node, nextTree.node);
+
+        if (isNaN(lastDist) || lastDist > curDist) {
+
+            nextTree.toRootNodeDist = curDist;
+            nextTree.parentTree = this;
+            this.childrenTree.push(nextTree);
+
+            this.rootTree.addTree(nextTree);
+            return true;
+        }
+    }
+
+    var RootNodeTree = function (startNode, endNode, mapData) {
+
+        this.mapData = mapData;
+        NodeTree.apply(this, [startNode, this]);
+        this.endNode = endNode;
+        this.nodeDistMap = {};
+        this.endTrees = [];
+        this.endNodes = [];
+    }
+    RootNodeTree.prototype = NodeTree.prototype;
+
+    //通过node获取到根节点的距离.
+    RootNodeTree.prototype.getDistToPoint = function (node) {
+        return this.nodeDistMap[node.id];
+    }
+    //通过node获取到根节点的距离.
+    RootNodeTree.prototype._setDistToPoint = function (nodeTree) {
+        this.nodeDistMap[nodeTree.node.id] = nodeTree.toRootNodeDist;
+    }
+    //处理末端Tree.
+    RootNodeTree.prototype._addEndNode = function (nodeTree) {
+
+        var ind = this.endNodes.indexOf(nodeTree.node);
+        if (-1 === ind) {
+            this.endNodes.push(nodeTree.node);
+            this.endTrees.push(nodeTree);
+        } else {
+            this.endTrees[ind] = nodeTree;
+        }
+    }
+    //通过node获取到根节点的距离.
+    RootNodeTree.prototype.addTree = function (nodeTree) {
+        this._setDistToPoint(nodeTree);
+        this._addEndNode(nodeTree);
+    }
+    //通过node获取到根节点的距离.
+    RootNodeTree.prototype._getMinTree = function () {
+        if (this.endTrees.length === 0) {
+            throw "no path found.";
+        }
+        var result = { toRootNodeDist: Infinity };
+        for (var i = this.endTrees.length - 1; i >= 0; i--) {
+
+            var now = this.endTrees[i]
+            if (now.node === this.endNode && now.toRootNodeDist < result.toRootNodeDist) {
+                result = now;
+            }
+        }
+        //没有课到达的路径.
+        if (Infinity === result.toRootNodeDist) {
+            throw "no path to dest node。";
+        }
+        return result;
+    }
+    RootNodeTree.prototype._loadParent = function (nodeTree, array) {
+        if (nodeTree) {
+            array.unshift(nodeTree.node);
+            this._loadParent(nodeTree.parentTree, array);
+        }
+    }
+    RootNodeTree.prototype.getMinPathNodes = function () {
+        var result = [];
+        this._loadParent(this._getMinTree(), result);
+        return result;
+    }
+    var findPath = function (pointA, pointD, map) {
+        var root = new RootNodeTree(pointA, pointD, map);
+        root.appendTree();
+        console.log(root);
+        return root.getMinPathNodes();
+    }
+    var PD = {
+        version: 1.0,
+        MapData: MapData,
+        Vectort2: Vectort2,
+        Segment: Segment,
+        findPath: findPath
+    }
+    if (window.PD) {
+        window._PD = PD;
+    } else {
+        window.PD = PD;
+    }
+})(window);
+
+
